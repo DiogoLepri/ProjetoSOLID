b099415fe9a39b6fcaef0c4b9b2027f03ef332b8
Diogo Lepri Moreira <124713632+DiogoLepri@users.noreply.github.com>
2025-04-09 18:11:53 -0300

    Create NovoPagamentoCartao.java

---
+++ src/OSolid/Exemplo2/NovoPagamentoCartao.java
@@ -0,0 +1,6 @@
+public class PagamentoCartao implements Pagamento {
+    @Override
+    public void realizarPagamento(double valor) {
+        System.out.println("Pagamento de R$" + valor + " realizado com CARTÃO.");
+    }
+}
