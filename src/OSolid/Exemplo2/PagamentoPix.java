c5c49b0bdefcf3c16c8fd99f97681b57de64a728
Diogo Lepri Moreira <124713632+DiogoLepri@users.noreply.github.com>
2025-04-09 18:13:27 -0300

    Create PagamentoPix.java

---
+++ src/OSolid/Exemplo2/PagamentoPix.java
@@ -0,0 +1,7 @@
+
+public class PagamentoPix implements Pagamento {
+    @Override
+    public void realizarPagamento(double valor) {
+        System.out.println("Pagamento de R$" + valor + " realizado via PIX.");
+    }
+}
